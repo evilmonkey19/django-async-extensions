5489a2a14570c8cd85553170902958923921280c
Enric Perpinyà Pitarch <enric@perpi.tech>
2026-01-07 20:07:16 +0100

    Checkpoint from VS Code for coding agent session

--- tests/test_generic_views/test_list.py
+++ tests/test_generic_views/test_list.py
@@ -4,17 +4,19 @@ import re
 import pytest
 
 from django.core.exceptions import ImproperlyConfigured
-from django.test import Client, TestCase
+from django.test.client import AsyncClient
+from django.test.utils import CaptureQueriesContext
+from django.db import connection
+from asgiref.sync import async_to_sync
 
 from django_async_extensions.views.generic.base import AsyncView
 
 from .models import Artist, Author, Book, Page
 
-client = Client()
-
+client = AsyncClient()
 
 @pytest.fixture(autouse=True)
-def url_setting_set(settings):
+async def url_setting_set(settings):
     old_root_urlconf = settings.ROOT_URLCONF
     settings.ROOT_URLCONF = "test_generic_views.urls"
     yield settings
@@ -22,75 +24,77 @@ def url_setting_set(settings):
 
 
 @pytest.mark.django_db(transaction=True)
-class ListViewTests(TestCase):
-    @classmethod
-    def setUpTestData(self):
-        self.artist1 = Artist.objects.create(name="Rene Magritte")
-        self.author1 = Author.objects.create(
+class TestListView:
+    @pytest.fixture(autouse=True)
+    async def setUpTestData(self, async_client: AsyncClient):
+        self.artist1 = await Artist.objects.acreate(name="Rene Magritte")
+        self.author1 = await Author.objects.acreate(
             name="Roberto Bolaño", slug="roberto-bolano"
         )
-        self.author2 = Author.objects.create(
+        self.author2 = await Author.objects.acreate(
             name="Scott Rosenberg", slug="scott-rosenberg"
         )
-        self.book1 = Book.objects.create(
+        self.book1 = await Book.objects.acreate(
             name="2066", slug="2066", pages=800, pubdate=datetime.date(2008, 10, 1)
         )
-        self.book1.authors.add(self.author1)
-        self.book2 = Book.objects.create(
+        await self.book1.authors.aadd(self.author1)
+        self.book2 = await Book.objects.acreate(
             name="Dreaming in Code",
             slug="dreaming-in-code",
             pages=300,
             pubdate=datetime.date(2006, 5, 1),
         )
-        self.page1 = Page.objects.create(
+        self.page1 = await Page.objects.acreate(
             content="I was once bitten by a moose.",
             template="generic_views/page_template.html",
         )
 
-    def test_items(self):
-        res = client.get("/list/dict/")
+    async def test_items(self):
+        res = await client.get("/list/dict/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/list.html"
         assert res.context["object_list"][0]["first"] == "John"
 
-    def test_queryset(self):
-        res = client.get("/list/authors/")
+    async def test_queryset(self):
+        res = await client.get("/list/authors/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert isinstance(res.context["view"], AsyncView)
         assert res.context["author_list"] is res.context["object_list"]
         assert res.context["paginator"] is None
         assert res.context["page_obj"] is None
         assert res.context["is_paginated"] is False
 
-    def test_paginated_queryset(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/")
+    async def test_paginated_queryset(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
         assert len(res.context["object_list"]) == 30
         assert res.context["author_list"] is res.context["object_list"]
         assert res.context["is_paginated"]
         assert res.context["page_obj"].number == 1
-        assert res.context["paginator"].num_pages == 4
+        assert (await res.context["paginator"].anum_pages()) == 4
         assert res.context["author_list"][0].name == "Author 00"
         assert list(res.context["author_list"])[-1].name == "Author 29"
 
-    def test_paginated_queryset_shortdata(self):
+    async def test_paginated_queryset_shortdata(self):
         # Short datasets also result in a paginated view.
-        res = client.get("/list/authors/paginated/")
+        res = await client.get("/list/authors/paginated/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert res.context["author_list"] is res.context["object_list"]
         assert res.context["page_obj"].number == 1
-        assert res.context["paginator"].num_pages == 1
+        assert (await res.context["paginator"].anum_pages()) == 1
         assert res.context["is_paginated"] is False
 
-    def test_paginated_get_page_by_query_string(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/", {"page": "2"})
+    async def test_paginated_get_page_by_query_string(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/", {"page": "2"})
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
         assert len(res.context["object_list"]) == 30
@@ -98,18 +102,18 @@ class ListViewTests(TestCase):
         assert res.context["author_list"][0].name == "Author 30"
         assert res.context["page_obj"].number == 2
 
-    def test_paginated_get_last_page_by_query_string(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/", {"page": "last"})
+    async def test_paginated_get_last_page_by_query_string(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/", {"page": "last"})
         assert res.status_code == 200
         assert len(res.context["object_list"]) == 10
         assert res.context["author_list"] is res.context["object_list"]
         assert res.context["author_list"][0].name == "Author 90"
         assert res.context["page_obj"].number == 4
 
-    def test_paginated_get_page_by_urlvar(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/3/")
+    async def test_paginated_get_page_by_urlvar(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/3/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
         assert len(res.context["object_list"]) == 30
@@ -117,27 +121,27 @@ class ListViewTests(TestCase):
         assert res.context["author_list"][0].name == "Author 60"
         assert res.context["page_obj"].number == 3
 
-    def test_paginated_page_out_of_range(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/42/")
+    async def test_paginated_page_out_of_range(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/42/")
         assert res.status_code == 404
 
-    def test_paginated_invalid_page(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/?page=frog")
+    async def test_paginated_invalid_page(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/?page=frog")
         assert res.status_code == 404
 
-    def test_paginated_custom_paginator_class(self):
-        self._make_authors(7)
-        res = client.get("/list/authors/paginated/custom_class/")
+    async def test_paginated_custom_paginator_class(self):
+        await self._make_authors(7)
+        res = await client.get("/list/authors/paginated/custom_class/")
         assert res.status_code == 200
-        assert res.context["paginator"].num_pages == 1
+        assert (await res.context["paginator"].anum_pages()) == 1
         # Custom pagination allows for 2 orphans on a page size of 5
         assert len(res.context["object_list"]) == 7
 
-    def test_paginated_custom_page_kwarg(self):
-        self._make_authors(100)
-        res = client.get("/list/authors/paginated/custom_page_kwarg/", {"pagina": "2"})
+    async def test_paginated_custom_page_kwarg(self):
+        await self._make_authors(100)
+        res = await client.get("/list/authors/paginated/custom_page_kwarg/", {"pagina": "2"})
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/author_list.html"
         assert len(res.context["object_list"]) == 30
@@ -145,138 +149,147 @@ class ListViewTests(TestCase):
         assert res.context["author_list"][0].name == "Author 30"
         assert res.context["page_obj"].number == 2
 
-    def test_paginated_custom_paginator_constructor(self):
-        self._make_authors(7)
-        res = client.get("/list/authors/paginated/custom_constructor/")
+    async def test_paginated_custom_paginator_constructor(self):
+        await self._make_authors(7)
+        res = await client.get("/list/authors/paginated/custom_constructor/")
         assert res.status_code == 200
         # Custom pagination allows for 2 orphans on a page size of 5
         assert len(res.context["object_list"]) == 7
 
-    def test_paginated_orphaned_queryset(self):
-        self._make_authors(92)
-        res = client.get("/list/authors/paginated-orphaned/")
+    async def test_paginated_orphaned_queryset(self):
+        await self._make_authors(92)
+        res = await client.get("/list/authors/paginated-orphaned/")
         assert res.status_code == 200
         assert res.context["page_obj"].number == 1
-        res = client.get("/list/authors/paginated-orphaned/", {"page": "last"})
+        res = await client.get("/list/authors/paginated-orphaned/", {"page": "last"})
         assert res.status_code == 200
         assert res.context["page_obj"].number == 3
-        res = client.get("/list/authors/paginated-orphaned/", {"page": "3"})
+        res = await client.get("/list/authors/paginated-orphaned/", {"page": "3"})
         assert res.status_code == 200
         assert res.context["page_obj"].number == 3
-        res = client.get("/list/authors/paginated-orphaned/", {"page": "4"})
+        res = await client.get("/list/authors/paginated-orphaned/", {"page": "4"})
         assert res.status_code == 404
 
-    def test_paginated_non_queryset(self):
-        res = self.client.get("/list/dict/paginated/")
+    async def test_paginated_non_queryset(self):
+        res = await client.get("/list/dict/paginated/")
 
         assert res.status_code == 200
         assert len(res.context["object_list"]) == 1
 
-    def test_verbose_name(self):
-        res = client.get("/list/artists/")
+    async def test_verbose_name(self):
+        res = await client.get("/list/artists/")
         assert res.status_code == 200
         assert res.template_name[0] == "test_generic_views/list.html"
-        assert list(res.context["object_list"]) == list(Artist.objects.all())
+        authors = [artist async for artist in Artist.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert res.context["artist_list"] is res.context["object_list"]
         assert res.context["paginator"] is None
         assert res.context["page_obj"] is None
         assert res.context["is_paginated"] is False
 
-    def test_allow_empty_false(self):
-        res = client.get("/list/authors/notempty/")
+    async def test_allow_empty_false(self):
+        res = await client.get("/list/authors/notempty/")
         assert res.status_code == 200
-        Author.objects.all().delete()
-        res = client.get("/list/authors/notempty/")
+        await Author.objects.all().adelete()
+        res = await client.get("/list/authors/notempty/")
         assert res.status_code == 404
 
-    def test_template_name(self):
-        res = client.get("/list/authors/template_name/")
+    async def test_template_name(self):
+        res = await client.get("/list/authors/template_name/")
         assert res.status_code == 200
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert res.context["author_list"] is res.context["object_list"]
         assert res.template_name[0] == "test_generic_views/list.html"
 
-    def test_template_name_suffix(self):
-        res = client.get("/list/authors/template_name_suffix/")
+    async def test_template_name_suffix(self):
+        res = await client.get("/list/authors/template_name_suffix/")
         assert res.status_code == 200
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert res.context["author_list"] is res.context["object_list"]
         assert res.template_name[0] == "test_generic_views/author_objects.html"
 
-    def test_context_object_name(self):
-        res = client.get("/list/authors/context_object_name/")
+    async def test_context_object_name(self):
+        res = await client.get("/list/authors/context_object_name/")
         assert res.status_code == 200
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert "authors" not in res.context
         assert res.context["author_list"] is res.context["object_list"]
         assert res.template_name[0] == "test_generic_views/author_list.html"
 
-    def test_duplicate_context_object_name(self):
-        res = client.get("/list/authors/dupe_context_object_name/")
+    async def test_duplicate_context_object_name(self):
+        res = await client.get("/list/authors/dupe_context_object_name/")
         assert res.status_code == 200
-        assert list(res.context["object_list"]) == list(Author.objects.all())
+        authors = [author async for author in Author.objects.all()]
+        assert list(res.context["object_list"]) == list(authors)
         assert "authors" not in res.context
         assert "author_list" not in res.context
         assert res.template_name[0] == "test_generic_views/author_list.html"
 
-    def test_missing_items(self):
+    async def test_missing_items(self):
         msg = (
             "AuthorList is missing a QuerySet. Define AuthorList.model, "
             "AuthorList.queryset, or override AuthorList.get_queryset()."
         )
         with pytest.raises(ImproperlyConfigured, match=msg):
-            client.get("/list/authors/invalid/")
+            await client.get("/list/authors/invalid/")
 
-    def test_invalid_get_queryset(self):
+    async def test_invalid_get_queryset(self):
         msg = re.escape(
             "AuthorListGetQuerysetReturnsNone requires either a 'template_name' "
             "attribute or a get_queryset() method that returns a QuerySet."
         )
         with pytest.raises(ImproperlyConfigured, match=msg):
-            client.get("/list/authors/get_queryset/")
+            await client.get("/list/authors/get_queryset/")
 
-    def test_paginated_list_view_does_not_load_entire_table(self):
+    def test_paginated_list_view_does_not_load_entire_table(self, client):
         # Regression test for #17535
-        self._make_authors(3)
+        async_to_sync(self._make_authors)(3)
         # 1 query for authors
-        with self.assertNumQueries(1):
+        with CaptureQueriesContext(connection) as ctx:
             client.get("/list/authors/notempty/")
+            assert len(ctx.captured_queries) == 1
+        
         # same as above + 1 query to test if authors exist + 1 query for pagination
-        with self.assertNumQueries(3):
+        with CaptureQueriesContext(connection) as ctx:
             client.get("/list/authors/notempty/paginated/")
+            assert len(ctx.captured_queries) == 3
 
-    def test_explicitly_ordered_list_view(self):
-        Book.objects.create(
+
+    async def test_explicitly_ordered_list_view(self):
+        await Book.objects.acreate(
             name="Zebras for Dummies", pages=800, pubdate=datetime.date(2006, 9, 1)
         )
-        res = self.client.get("/list/books/sorted/")
+        res = await client.get("/list/books/sorted/")
         assert res.status_code == 200
         assert res.context["object_list"][0].name == "2066"
         assert res.context["object_list"][1].name == "Dreaming in Code"
         assert res.context["object_list"][2].name == "Zebras for Dummies"
 
-        res = client.get("/list/books/sortedbypagesandnamedec/")
+        res = await client.get("/list/books/sortedbypagesandnamedec/")
         assert res.status_code == 200
         assert res.context["object_list"][0].name == "Dreaming in Code"
         assert res.context["object_list"][1].name == "Zebras for Dummies"
         assert res.context["object_list"][2].name == "2066"
 
     @pytest.fixture(autouse=True)
-    def toggle_debug_tests(self, settings):
+    async def toggle_debug_tests(self, settings):
         settings.DEBUG = True
 
-    def test_paginated_list_view_returns_useful_message_on_invalid_page(self):
+    async def test_paginated_list_view_returns_useful_message_on_invalid_page(self):
         # test for #19240
         # tests that source exception's message is included in page
-        self._make_authors(1)
-        res = client.get("/list/authors/paginated/2/")
+        await self._make_authors(1)
+        res = await client.get("/list/authors/paginated/2/")
         assert res.status_code == 404
         assert (
             res.context.get("reason")
             == "Invalid page (2): That page contains no results"
         )
 
-    def _make_authors(self, n):
-        Author.objects.all().delete()
+    async def _make_authors(self, n):
+        await Author.objects.all().adelete()
         for i in range(n):
-            Author.objects.create(name="Author %02i" % i, slug="a%s" % i)
+            await Author.objects.acreate(name="Author %02i" % i, slug="a%s" % i)
